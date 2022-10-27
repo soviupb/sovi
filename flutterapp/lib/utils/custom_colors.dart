efc7ad2c0d4c70f2e758abaa3cc5ad138a136138
Joseale58 <70922734+Joseale58@users.noreply.github.com>
2022-10-27 15:37:07 -0500

    ColorsUpdated

---
+++ flutterapp/lib/utils/custom_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color firebaseNavy = Color(0xFFA62E44);
+  static const Color firebaseOrange = Color(0xFF277365);
+  static const Color firebaseAmber = Color(0xFFF2B807);
+  static const Color firebaseYellow = Color(0xFFF28907);
+  static const Color firebaseGrey = Color(0xFFF27507);
+  static const Color googleBackground = Color(0xFF4285F4);
+  }
