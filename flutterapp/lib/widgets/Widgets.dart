2a4bafd57fa8abb42904fb6adf1fe39d50d32153
Camilo Restre <camilorestrerodriguez@gmail.com>
2022-10-28 14:39:23 -0500

    UNiversalBUtton3

--- flutterapp/lib/widgets/Widgets.dart
+++ flutterapp/lib/widgets/Widgets.dart
@@ -5,55 +5,6 @@ import 'package:flutterapp/screens/Inicio.dart';
 
 import '../utils/custom_colors.dart';
 
-GestureDetector UniversalButton(
-    VoidCallback function,
-    Color bgColor,
-    String text,
-    Color bgColorT,
-    double FontSize,
-    Color shadowColor,
-    Icon C,
-    double Height,
-    double Width) {
-  return GestureDetector(
-    onTap: function,
-    child: Container(
-      width: Width,
-      height: Height,
-      decoration: BoxDecoration(
-          color: bgColor,
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 5.0,
-              offset: Offset(0, 5),
-              color: shadowColor,
-            )
-          ]),
-      child: Center(
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            const Spacer(),
-            C, //C es el icono que se pase como parametro
-            const Spacer(flex: 2),
-            Text(
-              text,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: bgColorT,
-                fontSize: FontSize,
-              ),
-            ),
-            const Spacer(flex: 2),
-          ],
-        ),
-      ),
-    ),
-  );
-}
-
 GestureDetector SmallButton(VoidCallback function, Color bgColor, String text,
     Color bgColorT, Color shadowColor) {
   return GestureDetector(
