a04f2740600feeea1f438c3a435a3130f24a893b
santiac24 <104871424+santiac24@users.noreply.github.com>
2022-11-03 08:20:55 -0500

    LogosFbGgl

--- flutterapp/lib/screens/login.dart
+++ flutterapp/lib/screens/login.dart
@@ -19,73 +19,15 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
         body: Center(
       child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            color: Color.fromARGB(15, 255, 255, 255),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Column(
-            children: [
-              Spacer(
-                flex: 1,
-              ),
-              Text("Iniciar Sesión", style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),),
-              Spacer(
-                flex: 1,
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Row(
-                children: [
-                  SizedBox(
-                    width: 30,
-                  ),
-                  SizedBox(
-                    width: 50,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Correo",
-                  ),
-                ),
-              ),
-              SizedBox(height: 50),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Contraseña",
-                  ),
-                ),
-              ),
-              Spacer(),
-              UniversalButtonNoIcon(
-                  () {},
-                  CustomColors.SoviMagenta,
-                  "Iniciar sesión",
-                  Colors.white,
-                  15,
-                  CustomColors.SoviGris,
-                  50,
-                  MediaQuery.of(context).size.width * 0.8),
-              Spacer(flex: 1,),
-              Text("Crear Cuenta", style: TextStyle(color: CustomColors.SoviAzulMarino, fontSize: 20,fontStyle: FontStyle.italic),),
-              SizedBox(
-                height: 30,
-              ),
-              Text("¿Olvidaste tu contraseña?", style: TextStyle(color: CustomColors.SoviAzulMarino, fontSize: 20)),
-
-              SmallButton(() {
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: Color.fromARGB(15, 255, 255, 255),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: [
+            UniversalButton(() {
               Navigator.push(
                 context,
                 MaterialPageRoute(
@@ -94,15 +36,119 @@ class LoginPage extends StatelessWidget {
                   },
                 ),
               );
-            }, CustomColors.SoviMagenta, "Back", Colors.black,
-                CustomColors.SoviMagenta),
-              
-              Spacer(flex: 2),
-            ],
-            
-          ),
-          ),
-          
+            }, Colors.white, "", Colors.white, 10, Colors.white,
+                Icon(Icons.arrow_back), 50, 50),
+            Spacer(
+              flex: 3,
+            ),
+            /*SmallButton(() {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const InicioPage();
+                  },
+                ),
+              );
+            }, CustomColors.SoviMagenta, "<-", Colors.black,
+                CustomColors.SoviMagenta),*/
+
+            Text(
+              "Iniciar Sesión",
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            Spacer(
+              flex: 1,
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 30,
+                ),
+                SizedBox(
+                  width: 50,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Correo",
+                ),
+              ),
+            ),
+            SizedBox(height: 50),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Contraseña",
+                ),
+              ),
+            ),
+            Spacer(),
+            UniversalButtonNoIcon(
+                () {},
+                CustomColors.SoviMagenta,
+                "Iniciar sesión",
+                Colors.white,
+                15,
+                CustomColors.SoviGris,
+                50,
+                MediaQuery.of(context).size.width * 0.8),
+            Spacer(
+              flex: 1,
+            ),
+            Row(
+              children: [
+                Spacer(
+                  flex: 3,
+                ),
+                Icon(
+                  Icons.facebook,
+                  size: 60,
+                  color: Colors.blue,
+                ),
+                Spacer(
+                  flex: 1,
+                ),
+                Image(
+                  image: AssetImage("assets/google.png"),
+                  height: 50,
+                ),
+                Spacer(
+                  flex: 3,
+                )
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Crear Cuenta",
+              style: TextStyle(
+                  color: CustomColors.SoviAzulMarino,
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text("¿Olvidaste tu contraseña?",
+                style: TextStyle(
+                    color: CustomColors.SoviAzulMarino, fontSize: 20)),
+            Spacer(flex: 2),
+          ],
+        ),
+      ),
     ));
   }
 }
